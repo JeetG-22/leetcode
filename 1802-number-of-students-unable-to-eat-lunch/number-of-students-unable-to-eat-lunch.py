c705e5fa4e57bca1c72dd978e1739ea70f2ff999
Jeet <96960165+JeetG-22@users.noreply.github.com>
2026-01-22 01:49:34 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (8.09%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -2,11 +2,12 @@ from collections import deque
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         students = deque(students)
+        sandwiches = deque(sandwiches)
         length = len(sandwiches)
         i = 0
         while i < length:
             if students[0] == sandwiches[0]:
-                sandwiches.pop(0)
+                sandwiches.popleft()
                 students.popleft()
                 i = 0
                 length -= 1
